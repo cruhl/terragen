b330bf0081187961c48c24fa552f824557cd542d
Conner Ruhl <connerruhl@me.com>
2023-07-06 12:29:11 -0500

    Running

---
+++ packages/app/src/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "~/GlobalVariables";
+
+import { App } from "~/App";
+
+import "./index.css";
+
+const main = async () => {
+  const root = document.getElementById("app");
+  root &&
+    ReactDOM.createRoot(root).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+};
+
+main();
